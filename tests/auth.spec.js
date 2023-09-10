86f2bfb4e766825089f1892c51ace8026c1b22bb
Denis N <293728den@gmail.com>
2023-09-10 17:34:27 -0400

    Add environment variables

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,13 +1,26 @@
 const { test, expect } = require('@playwright/test');
+
 test.describe('Authentication & Authorization', () => {
+  test.beforeEach( async ({ page }) => {
+    await page.goto('/user/login')
+  })
   test('Sign in with existing credentials', async ({ page }) => {
-    await page.goto('https://coding.pasv.us/user/login')
-    await page.pause()
-    await page.locator("#normal_login_email").fill('293728den@gmail.com')
-    await page.locator('#normal_login_password').fill('293728den')
+
+    await page.locator("#normal_login_email").fill(process.env.EMAIL)
+    await page.locator('#normal_login_password').fill(process.env.PASSWORD)
     await page.locator('button[type="submit"]').click()
 
     await expect(page.locator('.ant-avatar-square')).toBeVisible()
 
+  })
+  test('Sign in with not existing credentials', async({ page}) => {
+
+    await page.locator("#normal_login_email").fill('abc@gmail.com')
+    await page.locator('#normal_login_password').fill('123')
+    await page.locator('button[type="submit"]').click()
+
+    const toast = page.locator('.ant-notification-notice-content')
+    await expect(toast).toBeVisible()
+    await expect(toast).toHaveText('User login. Fail')
   })
 })
